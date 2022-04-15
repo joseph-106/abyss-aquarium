25ab128cbcc8d9f4dba26b072accf9ee0a04b388
unknown <sjjoseph106@gmail.com>
2022-04-15 21:43:13 +0900

    Refactor: 자잘한 수정

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 const canvas = <HTMLCanvasElement>document.getElementById("canvas");
 const ctx: CanvasRenderingContext2D = canvas.getContext("2d");
 let animation: number;
-let timer = 0;
 let flag = 1;
 
 const fish = {
@@ -16,21 +15,17 @@ const fish = {
 };
 
 function displaySea() {
+  // 바다 배경
   animation = requestAnimationFrame(displaySea);
-  timer++;
   canvas.width = window.innerWidth;
   canvas.height = 10000;
-  const lineargradient = ctx.createLinearGradient(
-    0,
-    300,
-    canvas.width,
-    canvas.height
-  );
+  const lineargradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
   lineargradient.addColorStop(0, "#055cfc");
   lineargradient.addColorStop(0.6, "black");
   ctx.fillStyle = lineargradient;
   ctx.fillRect(0, 300, canvas.width, canvas.height);
-  if (flag === 1) {
+  // 물고기 이동
+  if (flag) {
     fish.x += 1;
     if (fish.x > canvas.width - fish.width) {
       flag = 0;
